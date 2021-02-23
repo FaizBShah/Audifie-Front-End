9452085f28eb8d6e752af6a25d6c299fd9044acd
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2021-02-23 13:34:37 +0530

    Uninstalled antd, installed Material-UI

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { Button } from '@material-ui/core'
 
 function Home() {
   return (
     <div style={{height: "100vh", width: "100%"}}>
       <img style={{width: "100%"}} src="assets/kit/coverAsset@10x.png"></img>
+      <Button variant="contained" color="secondary">Default</Button>
     </div>
   )
 }
