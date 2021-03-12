58d5e4752679f3ea688a3bc8dd8dcd4891aa8af6
shafa45 <safaullah14@gmail.com>
2021-03-12 11:23:27 +0530

    Link added to logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import { Container, Link } from '@material-ui/core';
-import { NavAppBar, NavToolBar, NavDrawer } from '../components/MaterialComponents';
+import {
+  NavAppBar,
+  NavToolBar,
+  NavDrawer,
+} from '../components/MaterialComponents';
 import styles from '../styles/Navbar.module.css';
 import { useWindowDimensions } from '../common/windowUtils';
 
@@ -10,21 +14,24 @@ const Navbar = () => {
 
   const toggleDrawer = (open) => {
     setIsDrawerOpen(open);
-  }
+  };
 
   return (
     <NavAppBar>
       <Container maxWidth='lg'>
         <NavToolBar>
-          <img
-            src='/assets/kit/logo_linear.png'
-            alt=''
-            className={styles.brandLogo}
-            style={{
-              height: '3rem',
-              margin: width <= 768 ? '1.25rem auto' : '0',
-            }}
-          ></img>
+          <Link href='/'>
+            <img
+              src='/assets/kit/logo_linear.png'
+              alt=''
+              className={styles.brandLogo}
+              style={{
+                height: '3rem',
+                margin: width <= 768 ? '1.25rem auto' : '0',
+              }}
+            ></img>
+          </Link>
+
           {width >= 769 ? (
             <div className={styles.navLink}>
               <Link href='/signup'>Sign Up</Link>
@@ -32,19 +39,22 @@ const Navbar = () => {
             </div>
           ) : (
             <div>
-              <img src="/assets/svg/menu-bars.svg" style={{color: "#fd5457", height: '1.25rem'}} onClick={() => toggleDrawer(true)}></img>
-              <NavDrawer 
-                anchor={'bottom'} 
-                open={isDrawerOpen} 
-                onClose={() => toggleDrawer(false)}>
-                  <div
-                    role="presentation"
-                    onClick={() => toggleDrawer(false)}>
-                      <div className={styles.navLink}>
-                        <Link href='/signup'>Sign Up</Link>
-                        <Link href='/login'>Sign In</Link>
-                      </div>
+              <img
+                src='/assets/svg/menu-bars.svg'
+                style={{ color: '#fd5457', height: '1.25rem' }}
+                onClick={() => toggleDrawer(true)}
+              ></img>
+              <NavDrawer
+                anchor={'bottom'}
+                open={isDrawerOpen}
+                onClose={() => toggleDrawer(false)}
+              >
+                <div role='presentation' onClick={() => toggleDrawer(false)}>
+                  <div className={styles.navLink}>
+                    <Link href='/signup'>Sign Up</Link>
+                    <Link href='/login'>Sign In</Link>
                   </div>
+                </div>
               </NavDrawer>
             </div>
           )}
