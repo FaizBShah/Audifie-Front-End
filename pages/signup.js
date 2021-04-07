4b1f6180f1a0b060c55a19aa6d67600c929e869b
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2021-04-07 15:55:10 +0530

    Made the Verify OTP page

--- pages/signup.js
+++ pages/signup.js
@@ -13,6 +13,7 @@ function Signup() {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [isErrorVisible, setIsErrorVisible] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [isCodeWaiting, setIsCodeWaiting] = useState(false);
   const [errors, setErrors] = useState({
     name: '',
     email: '',
@@ -66,73 +67,104 @@ function Signup() {
                       <img src='/assets/kit/logo_linear.png' className={styles.brandLogo}/>
                     </Link>
                     <div className={styles.formBody}>
-                      <form className={styles.form}>
-                        <PrimaryInput  
-                          type="text" 
-                          label="Name" 
-                          variant="outlined"
-                          style={{marginBottom: "2rem"}}
-                          value={name}
-                          helperText={errors.name}
-                          onChange={(e) => setName(e.target.value)}/>
-                        <PrimaryInput  
-                          type="email" 
-                          label="Email" 
-                          variant="outlined"
-                          style={{marginBottom: "2rem"}}
-                          value={email}
-                          helperText={errors.email}
-                          onChange={(e) => setEmail(e.target.value)}/>
-                        <PrimaryInput  
-                          type="password" 
-                          label="Password" 
-                          variant="outlined"
-                          style={{marginBottom: "2rem"}}
-                          value={password}
-                          helperText={errors.password}
-                          onChange={(e) => setPassword(e.target.value)}/>
-                        <PrimaryInput  
-                          type="password" 
-                          label="Confirm Password" 
-                          variant="outlined"
-                          style={{marginBottom: "2rem"}}
-                          value={confirmPassword}
-                          helperText={errors.confirmPassword}
-                          onChange={(e) => setConfirmPassword(e.target.value)}/>
-                        <PrimaryButton
-                          size="medium"
-                          variant="contained"
-                          style={{width: "100%"}}
-                          onClick={handleSignUp}>
-                          Sign Up
-                        </PrimaryButton>
-                      </form>
-                      <div className={styles.dividerArea}>
-                        <div className={styles.divider}></div>
-                          <div className={styles.dividerText}>
-                            or Sign in with
+                      {!isCodeWaiting ? (
+                        <>
+                          <form className={styles.form}>
+                            <PrimaryInput  
+                              type="text" 
+                              label="Name" 
+                              variant="outlined"
+                              style={{marginBottom: "2rem"}}
+                              value={name}
+                              helperText={errors.name}
+                              onChange={(e) => setName(e.target.value)}/>
+                            <PrimaryInput  
+                              type="email" 
+                              label="Email" 
+                              variant="outlined"
+                              style={{marginBottom: "2rem"}}
+                              value={email}
+                              helperText={errors.email}
+                              onChange={(e) => setEmail(e.target.value)}/>
+                            <PrimaryInput  
+                              type="password" 
+                              label="Password" 
+                              variant="outlined"
+                              style={{marginBottom: "2rem"}}
+                              value={password}
+                              helperText={errors.password}
+                              onChange={(e) => setPassword(e.target.value)}/>
+                            <PrimaryInput  
+                              type="password" 
+                              label="Confirm Password" 
+                              variant="outlined"
+                              style={{marginBottom: "2rem"}}
+                              value={confirmPassword}
+                              helperText={errors.confirmPassword}
+                              onChange={(e) => setConfirmPassword(e.target.value)}/>
+                            <PrimaryButton
+                              size="medium"
+                              variant="contained"
+                              style={{width: "100%"}}
+                              onClick={handleSignUp}>
+                              Sign Up
+                            </PrimaryButton>
+                          </form>
+                          <div className={styles.dividerArea}>
+                            <div className={styles.divider}></div>
+                              <div className={styles.dividerText}>
+                                or Sign in with
+                              </div>
+                            <div className={styles.divider}></div>
                           </div>
-                        <div className={styles.divider}></div>
-                      </div>
-                      <div className={styles.federatedArea}>
-                        <GoogleButton
-                          startIcon={<img src="/assets/Icons/google.svg" style={{height:'1.15rem'}}/>}
-                          size="medium"
-                          type="contained"
-                          style={{width: "100%", flex: "1", marginBottom: "1rem", marginRight: width > 768 ? "0.5rem" : "0"}}>
-                            Google
-                        </GoogleButton>
-                        <FacebookButton
-                          startIcon={<i className="fab fa-facebook"></i>}
-                          size="medium"
-                          type="contained"
-                          style={{width: "100%", flex: "1", marginBottom: "1rem"}}>
-                            Facebook
-                        </FacebookButton>
-                      </div>
-                      <div className={styles.bottomText}>
-                        Don't have an account? <Link href='/login'>Sign In here</Link>
-                      </div>
+                          <div className={styles.federatedArea}>
+                            <GoogleButton
+                              startIcon={<img src="/assets/Icons/google.svg" style={{height:'1.15rem'}}/>}
+                              size="medium"
+                              type="contained"
+                              style={{width: "100%", flex: "1", marginBottom: "1rem", marginRight: width > 768 ? "0.5rem" : "0"}}>
+                                Google
+                            </GoogleButton>
+                            <FacebookButton
+                              startIcon={<i className="fab fa-facebook"></i>}
+                              size="medium"
+                              type="contained"
+                              style={{width: "100%", flex: "1", marginBottom: "1rem"}}>
+                                Facebook
+                            </FacebookButton>
+                          </div>
+                          <div className={styles.bottomText}>
+                            Don't have an account? <Link href='/login'>Sign In here</Link>
+                          </div>
+                        </>
+                      ) : (
+                        <>
+                          <div className={styles.codeHeading}>
+                            <h4>OTP has been sent to your email.</h4>
+                            <h4>Enter OTP</h4>
+                          </div>
+                          <form className={styles.form} style={{marginTop: "0"}}>
+                            <PrimaryInput  
+                                type="text" 
+                                label="Verfication Code" 
+                                variant="outlined"/>
+                          </form>
+                          <div className={styles.federatedArea} style={{display: 'block'}}>
+                            <PrimaryButton
+                              size="medium"
+                              type="contained"
+                              style={{width: "100%", marginBottom: "1rem"}}>
+                                Submit
+                            </PrimaryButton>
+                            <PrimaryButton
+                              size="medium"
+                              type="contained"
+                              style={{width: "100%", marginBottom: "1rem"}}>
+                                Resend Code
+                            </PrimaryButton>
+                          </div>
+                        </>
+                      )}
                     </div>
                   </div>
               </Grid>
