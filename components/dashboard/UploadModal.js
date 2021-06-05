3b11c801938d47af6dafce6eb531372511355086
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2021-06-05 19:42:37 +0530

    Implemented File Browsing feature

--- components/dashboard/UploadModal.js
+++ components/dashboard/UploadModal.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { PrimaryDialog, PrimaryDialogContent, PrimaryButton, SecondaryButton } from '../MaterialComponents';
 import styles from '../../styles/UploadModal.module.css';
 
 function UploadModal({ open, setIsModalOpen }) {
+  const fileRef = useRef(null);
+
+  const onBrowseFiles = () => {
+    if (fileRef && fileRef.current) {
+      fileRef.current.click();
+    }
+  }
+
+  const onHandleDrag = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
+  const onHandleFileDrop = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    
+    const dt = e.dataTransfer;
+    const file = dt.files[0];
+
+    handleFile(file);
+  }
+
+  const handleFile = (file) => {
+    if (file.type === 'application/pdf') {
+      console.log(file);
+    }
+  }
+
   return (
     <>
       <PrimaryDialog
@@ -14,14 +43,26 @@ function UploadModal({ open, setIsModalOpen }) {
         disableEscapeKeyDown={true}
       >
         <PrimaryDialogContent>
-          <div className={styles.uploadArea}>
+          <div
+            className={styles.uploadArea} 
+            onDragEnter={onHandleDrag} 
+            onDragOver={onHandleDrag} 
+            onDrop={onHandleFileDrop}
+          >
             <h4 className={styles.uploadText}>Drag & Drop your files here</h4>
             <img src="/assets/svg/uploadFileLogo.svg" className={styles.uploadLogo}/>
           </div>
+          <input
+            ref={fileRef} 
+            type="file" 
+            style={{display: 'none'}} 
+            onChange={(e) => handleFile(e.target.files[0])}
+          />
           <PrimaryButton
             size="small"
             variant="contained"
             style={{marginTop: '2rem', marginLeft: 'auto', marginRight: 'auto'}}
+            onClick={onBrowseFiles}
           >
             Browse Files
           </PrimaryButton>
