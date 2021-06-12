39692ab332c4479d957732f52dbf275e48d8a658
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2021-06-12 16:25:00 +0530

    Added frosted glass effect on small player

--- components/player/Player.js
+++ components/player/Player.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Player() {
   return (
     <>
-      <div style={{background: 'red', height: '100%'}}></div>
+      <div style={{height: '100%'}}></div>
     </>
   )
 }
