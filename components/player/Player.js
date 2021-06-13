5a453764c2a00a7856f6f1133cadbec550982c67
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2021-06-13 21:09:48 +0530

    Made the UI top part of the large screen player

--- components/player/Player.js
+++ components/player/Player.js
@@ -8,7 +8,23 @@ function Player() {
     <>
       <div className={styles.mainArea}>
         <Container maxWidth="lg" style={{height: "100%"}}>
+          <div className={styles.innerArea}>
+            <div className={styles.nameArea}>
+              <h3 className={styles.title}>
+                <span><i style={{display: "inline-block", marginLeft: "-4rem", cursor: "pointer"}} className={"fas fa-chevron-down"}></i></span>
+                {' '}
+                &nbsp;
+                &nbsp;
+                Forever is True
+              </h3>
+            </div>
+            <div className={styles.imageArea}>
+              <img className={styles.playerImage} style={{background: "url('/assets/kit/harryPotter.jpg') no-repeat center center/cover"}} />
+            </div>
+            <div className={styles.controlArea}>
 
+            </div>
+          </div>
         </Container>
       </div>
     </>
