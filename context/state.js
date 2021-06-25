8f57c74067aedac910a93fec69e206a10c81f301
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2021-06-25 20:57:00 +0530

    Added useReducer in the state.js file to add state management support to the app

--- context/state.js
+++ context/state.js
@@ -1,12 +1,19 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useReducer } from 'react';
 
 const AppContext = createContext();
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
 export function AppWrapper({ children }) {
-  let sharedState = {};
+  const [sharedState, dispatch] = useReducer(reducer, {});
 
   return (
-    <AppContext.Provider value={sharedState}>
+    <AppContext.Provider value={sharedState, dispatch}>
       {children}
     </AppContext.Provider>
   )
