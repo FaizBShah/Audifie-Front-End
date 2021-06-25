3ba90c3c3ba13ce1d400ed14e04eafe7276cb49c
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2021-06-25 20:48:26 +0530

    Created a global state context using createContext and useContext

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../theme/theme';
 import '../styles/CustomIcons.css'
+import { AppWrapper } from '../context/state';
 import Amplify from 'aws-amplify';
 import awsConfig from '../config/awsConfig';
 
@@ -31,11 +32,13 @@ function MyApp(props) {
         <title>Audifie</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <AppWrapper>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </AppWrapper>
     </React.Fragment>
   );
 }
